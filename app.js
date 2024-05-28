f1092b247efa468a75ad0fc37c7fe1f3124a0914
Christoph <c.wagner.germany@gmail.com>
2024-05-28 20:41:32 +0200

    28May4

--- app.js
+++ app.js
@@ -16,17 +16,17 @@ const setTimer = duraction => {
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
     posData => {
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      setTimer(2000).then(data => {
+        console.log(data, posData);
+      });
     },
     error => {
       console.log(error);
     }
   );
-  setTimeout(() => {
-    console.log('Timer done!');
-  }, 0);
+  setTimer(1000).then(() => {
+    console.log('Timer done');
+  })
   console.log('Getting position...');
 }
 
