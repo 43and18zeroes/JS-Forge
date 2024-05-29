d598ab6045d142b36d74a3c4468dd41474c02dc8
Christoph <c.wagner.germany@gmail.com>
2024-05-29 18:38:23 +0200

    29May5

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
+// It takes a couple a bit of time to log the result. The eventlistener can’t be fired until the loop is complete.
+// Single-Threading in action: The eventlistener is blocked until the operations in the loop are done.
+
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-const setTimer = duraction => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Done');
-    }, duraction);
-  });
-  return promise;
-};
-
-function trackUserHandler() {
-  navigator.geolocation.getCurrentPosition(
-    posData => {
-      setTimer(2000).then(data => {
-        console.log(data, posData);
-      });
-    },
-    error => {
-      console.log(error);
-    }
-  );
-  setTimer(1000).then(() => {
-    console.log('Timer done');
-  });
-  console.log('Getting position...');
+function startAdding() {
+  let number = 0;
+  setTimeout(() => {
+    number++;
+    console.log('First number: ', number)
+  }, 1000);
+  setTimeout(() => {
+    number++;
+    console.log('Second number: ', number)
+  }, 2000);
 }
 
-button.addEventListener('click', trackUserHandler); // Callback Function
+button.addEventListener('click', startAdding); // Callback Function
+
+// let result = 0;
+
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
+
+// console.log(result);
