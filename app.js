6489bae7f3bbed9beb1397aea3f8c44a2b021cf7
Christoph <c.wagner.germany@gmail.com>
2024-05-29 18:10:29 +0200

    29May4

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = duraction => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done');
+    }, duraction);
+  });
+  return promise;
+};
+
 function trackUserHandler() {
-  setTimeout(() => {
-    console.log('1000ms');
-  }, 1000);
+  navigator.geolocation.getCurrentPosition(
+    posData => {
+      setTimer(2000).then(data => {
+        console.log(data, posData);
+      });
+    },
+    error => {
+      console.log(error);
+    }
+  );
+  setTimer(1000).then(() => {
+    console.log('Timer done');
+  });
+  console.log('Getting position...');
 }
 
 button.addEventListener('click', trackUserHandler); // Callback Function
