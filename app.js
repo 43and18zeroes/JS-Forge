f7f86b3cb3ffa9704087f7c0887bb30c7640d36d
Christoph <c.wagner.germany@gmail.com>
2024-06-05 23:31:43 +0200

    5June3

--- app.js
+++ app.js
@@ -68,3 +68,14 @@ function repeatedGreeting() {
 }
 
 repeatedGreeting();
+
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Data fetched');
+    }, 2000);
+  });
+}
+
+fetchData().then(data => console.log(data));
