1be8f762c10bee31471ea8f246e8439816a74b51
Christoph <c.wagner.germany@gmail.com>
2024-06-06 19:05:57 +0200

    6June10

--- app.js
+++ app.js
@@ -1,53 +1,21 @@
-// It takes a couple a bit of time to log the result. The eventlistener can’t be fired until the loop is complete.
-// Single-Threading in action: The eventlistener is blocked until the operations in the loop are done.
+const button = document.querySelector('button')
+const output = document.querySelector('p')
 
-const button = document.querySelector('button');
-const output = document.querySelector('p');
-
-function startAdding() {
-  let number = 0;
-  setTimeout(() => {
-    number++;
-    console.log('First number: ', number);
-    if (number === 1) {
+function trackUserHandler() {
+  navigator.geolocation.getCurrentPosition(
+    posData => {
       setTimeout(() => {
-        number++;
-        console.log('Second number: ', number);
-      }, 1000);
-    } else console.log("Second operation wasn't executed");
-  }, 1000);
-}
-
-function operation1(number) {
+        console.log(posData);
+      }, 2000)
+    },
+    error => {
+      console.log(error);
+    }
+  );
   setTimeout(() => {
-    number++;
-    console.log('First number: ', number);
-    return number;
-  }, 1000);
+    console.log('Time done!');
+  }, 0);
+  console.log('Getting position...')
 }
 
-function operation2(number) {
-  setTimeout(() => {
-    number++;
-    console.log('Second number: ', number);
-    return number;
-  }, 1000);
-}
-
-function startSequence() {
-  let number = 0;
-  number = operation1(number);
-  setTimeout(() => {
-    operation2(number);
-  }, 1500);
-}
-
-button.addEventListener('click', startSequence); // Callback Function
-
-// let result = 0;
-
-// for (let i = 0; i < 100000000; i++) {
-//   result += i;
-// }
-
-// console.log(result);
+button.addEventListener('click', trackUserHandler);
