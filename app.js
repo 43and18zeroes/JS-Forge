36764e49a0373aa6853452814e26b53524dd293c
Christoph <c.wagner.germany@gmail.com>
2024-06-06 19:00:01 +0200

    6June9

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
-const recordVideoOne = new Promise((resolve, reject) => {
-  resolve('Video 1 Recorded');
-});
+// It takes a couple a bit of time to log the result. The eventlistener can’t be fired until the loop is complete.
+// Single-Threading in action: The eventlistener is blocked until the operations in the loop are done.
 
-const recordVideoTwo = new Promise((resolve, reject) => {
-  resolve('Video 2 Recorded');
-});
+const button = document.querySelector('button');
+const output = document.querySelector('p');
 
-const recordVideoThree = new Promise((resolve, reject) => {
-  resolve('Video 3 Recorded');
-});
+function startAdding() {
+  let number = 0;
+  setTimeout(() => {
+    number++;
+    console.log('First number: ', number);
+    if (number === 1) {
+      setTimeout(() => {
+        number++;
+        console.log('Second number: ', number);
+      }, 1000);
+    } else console.log("Second operation wasn't executed");
+  }, 1000);
+}
 
-Promise.all([recordVideoOne, recordVideoTwo, recordVideoThree]).then(
-  messages => {
-    console.log(messages); // ['Video 1 Recorded', 'Video 2 Recorded', 'Video 3 Recorded']
-  }
-);
+function operation1(number) {
+  setTimeout(() => {
+    number++;
+    console.log('First number: ', number);
+    return number;
+  }, 1000);
+}
+
+function operation2(number) {
+  setTimeout(() => {
+    number++;
+    console.log('Second number: ', number);
+    return number;
+  }, 1000);
+}
+
+function startSequence() {
+  let number = 0;
+  number = operation1(number);
+  setTimeout(() => {
+    operation2(number);
+  }, 1500);
+}
+
+button.addEventListener('click', startSequence); // Callback Function
+
+// let result = 0;
+
+// for (let i = 0; i < 100000000; i++) {
+//   result += i;
+// }
+
+// console.log(result);
