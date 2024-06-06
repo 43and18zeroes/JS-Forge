69f5fd786a6e593011121ebfb2cd065046928804
Christoph <c.wagner.germany@gmail.com>
2024-06-06 18:26:34 +0200

    6June3

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let p = new Promise((resolve, reject) => {
-  let a = 1 + 1;
+  let a = 1 + 2; // Change
   if (a == 2) {
-    resolve('Success')
+    resolve('Success');
   } else {
-    reject('Failed')
+    reject('Failed');
   }
+});
+
+p.then(message => {
+  console.log('This is in the then ' + message); // This is in the catch Failed
+}).catch(message => {
+  console.log('This is in the catch ' + message);
 });
