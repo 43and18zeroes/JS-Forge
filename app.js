aee1900cbce5e3b13023cb67b595d87b8c0921f8
Christoph <c.wagner.germany@gmail.com>
2024-06-06 18:21:40 +0200

    6June8

--- app.js
+++ app.js
@@ -1,53 +1,8 @@
-// It takes a couple a bit of time to log the result. The eventlistener can’t be fired until the loop is complete.
-// Single-Threading in action: The eventlistener is blocked until the operations in the loop are done.
-
-const button = document.querySelector('button');
-const output = document.querySelector('p');
-
-function startAdding() {
-  let number = 0;
-  setTimeout(() => {
-    number++;
-    console.log('First number: ', number);
-    if (number === 1) {
-      setTimeout(() => {
-        number++;
-        console.log('Second number: ', number);
-      }, 1000);
-    } else console.log("Second operation wasn't executed");
-  }, 1000);
-}
-
-function operation1(number) {
-  setTimeout(() => {
-    number++;
-    console.log('First number: ', number);
-    return number;
-  }, 1000);
-}
-
-function operation2(number) {
-  setTimeout(() => {
-    number++;
-    console.log('Second number: ', number);
-    return number;
-  }, 1000);
-}
-
-function startSequence() {
-  let number = 0;
-  number = operation1(number);
-  setTimeout(() => {
-    operation2(number);
-  }, 1500);
-}
-
-button.addEventListener('click', startSequence); // Callback Function
-
-// let result = 0;
-
-// for (let i = 0; i < 100000000; i++) {
-//   result += i;
-// }
-
-// console.log(result);
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1;
+  if (a == 2) {
+    resolve('Success')
+  } else {
+    reject('Failed')
+  }
+});
