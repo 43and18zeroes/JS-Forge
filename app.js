0b4d796e0e39de9db1aaa8bd0fd04d55851c95ec
Christoph <c.wagner.germany@gmail.com>
2024-06-08 22:20:11 +0200

    8June3

--- app.js
+++ app.js
@@ -58,3 +58,19 @@ fetchData()
   .catch((error) => {
     console.error('Error:', error);
   });
+
+
+  async function* asyncGenerator() {
+    const data = [1, 2, 3];
+    for (const item of data) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      yield item;
+    }
+  }
+  
+  (async () => {
+    for await (const value of asyncGenerator()) {
+      console.log(value); // Outputs 1, 2, 3 each after a 1-second delay
+    }
+  })();
+  
