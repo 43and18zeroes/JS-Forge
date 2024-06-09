674e4800c2394b79e067f7cdfb85710e939f5972
Christoph <c.wagner.germany@gmail.com>
2024-06-09 15:45:25 +0200

    9June8

--- app.js
+++ app.js
@@ -94,3 +94,16 @@ xhr.onreadystatechange = function () {
   }
 };
 xhr.send();
+
+
+console.log('Start');
+
+setTimeout(() => {
+  console.log('Timeout');
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log('Promise');
+});
+
+console.log('End');
