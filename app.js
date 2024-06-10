06d0b2ae8922ec93577621b698a6ec94c525109a
Christoph <c.wagner.germany@gmail.com>
2024-06-10 20:35:17 +0200

    10June6

--- app.js
+++ app.js
@@ -68,3 +68,9 @@ async function fetchData() {
 }
 
 fetchData();
+
+
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
