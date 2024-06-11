2d527f435ac1e9c7af4d99c525e6c39795559bb9
Christoph <c.wagner.germany@gmail.com>
2024-06-11 18:20:51 +0200

    11June11

--- app.js
+++ app.js
@@ -42,12 +42,17 @@ function trackUserHandler() {
 button.addEventListener('click', trackUserHandler);
 
 function getWeather() {
-  return new Promise(function(resolve, reject) {
-    resolve('Sunny')
-  })
+  return new Promise(function (resolve, reject) {
+    reject('Sunny');
+  });
 }
 
 const promise = getWeather();
-promise.then(function(data) {
-  console.log(data);
-})
+promise.then(
+  function (data) {
+    console.log(`First param ${data}`);
+  },
+  function (data) {
+    console.log(`Second param ${data}`);
+  }
+);
