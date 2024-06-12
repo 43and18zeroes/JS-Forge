4b9e517a028715f1b75eb5dd017ae36a77b437b5
Christoph <c.wagner.germany@gmail.com>
2024-06-12 18:10:18 +0200

    12June8

--- app.js
+++ app.js
@@ -41,3 +41,31 @@ function trackUserHandler() {
 
 button.addEventListener('click', trackUserHandler);
 
+function fun1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject('404');
+    }, 100);
+  });
+}
+
+function fun2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Mustache Emoji');
+    }, 100);
+  });
+}
+
+function onSuccess(data) {
+  console.log(data);
+}
+
+function onError(errorCode) {
+  console.log(`ERROR: ${errorCode}`);
+}
+
+fun1()
+  .then(fun2)
+  .then(onSuccess)
+  .catch(onError)
