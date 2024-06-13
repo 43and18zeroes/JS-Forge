697612c760ba667874d18cf812dac77fbe2e930b
Christoph <c.wagner.germany@gmail.com>
2024-06-13 18:40:44 +0200

    13June10

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function trackUserHandler() {
 
 button.addEventListener('click', trackUserHandler);
 
-if ("geolocation" in navigator) {
+if ('geolocation' in navigator) {
   // Funktion zum Abrufen des aktuellen Standorts
   const getCurrentLocation = async () => {
     try {
@@ -58,26 +58,34 @@ if ("geolocation" in navigator) {
       longitude = Number(longitude.toFixed(0));
 
       // Standortinformationen in der Konsole ausgeben
-      console.log("Aktueller Standort:");
+      console.log('Aktueller Standort:');
       console.log(`Breitengrad: ${latitude}`);
       console.log(`Längengrad: ${longitude}`);
+
+      // Wetterdaten abrufen und anzeigen
+      fetchData(latitude, longitude).then(displayData).catch(onError);
     } catch (error) {
       // Fehlerbehandlung
-      console.error("Fehler beim Abrufen des Standorts: ", error.message);
+      console.error('Fehler beim Abrufen des Standorts: ', error.message);
     }
   };
 
   // Standort abrufen
   getCurrentLocation();
 } else {
-  console.error("Geolocation wird von diesem Browser nicht unterstützt.");
+  console.error('Geolocation wird von diesem Browser nicht unterstützt.');
 }
 
-function fetchData() {
+function fetchData(latitude, longitude) {
+  latitudeStr = latitude.toString();
+  longitudeStr = longitude.toString();
   return new Promise(function (resolve, reject) {
-    fetch(`https://api.weather.gov/gridpoints/OKX/${latitude},${longitude}/forecast`)
+    fetch(
+      `https://api.weather.gov/gridpoints/OKX/35,35/forecast`
+    )
       .then(response => response.json())
-      .then(data => resolve(data.properties.periods[1].shortForecast));
+      .then(data => resolve(data.properties.periods[1].shortForecast))
+      .catch(error => reject(error));
   });
 }
 
@@ -88,7 +96,3 @@ function displayData(weather) {
 function onError(err) {
   console.log(`ERROR: ${err}`);
 }
-
-fetchData()
-  .then(displayData)
-  .catch(onError)
