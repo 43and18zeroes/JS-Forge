2f0ba06c7fbac14b0b298930c8bd468887400e76
Christoph <c.wagner.germany@gmail.com>
2024-06-14 18:00:50 +0200

    weather working9

--- weather.js
+++ weather.js
@@ -1,9 +1,15 @@
-function fetchData() {
-  return new Promise(function (resolve, reject) {
-    fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
-      .then(response => response.json())
-      .then(data => resolve(data.properties.periods[1].shortForecast));
+function getCoordinates() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
+}
+
+function fetchData(latitude, longitude) {
+  const apiKey = 'd9425257f99d954ecdc16740272490d5';
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data.weather[0].description); // You can adjust this to get other weather details if needed
 }
 
 function displayData(weather) {
@@ -11,9 +17,13 @@ function displayData(weather) {
 }
 
 function onError(err) {
-  console.log(`ERROR: ${err}`);
+  console.log(`ERROR: ${err.message}`);
 }
 
-fetchData()
+getCoordinates()
+  .then(position => {
+    const { latitude, longitude } = position.coords;
+    return fetchData(latitude, longitude);
+  })
   .then(displayData)
   .catch(onError);
