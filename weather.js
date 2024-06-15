8391b1c39f4381f989848e942ce0777176e5b1e9
Christoph <c.wagner.germany@gmail.com>
2024-06-15 15:15:13 +0200

    inheritance10

--- weather.js
+++ weather.js
@@ -1,19 +1,29 @@
-class Person {
+// class Person {
+//   talk() {
+//     return 'Talking';
+//   }
+// }
+
+// const me = new Person();
+// console.log(me.talk());
+
+// class SuperHuman extends Person {
+//   fly() {
+//     return 'Flying';
+//   }
+// }
+
+// const you = new SuperHuman();
+// console.log(you.fly());
+// console.log(you.talk());
+// console.log(me.fly());
+
+const person = {
   talk() {
     return 'Talking';
   }
 }
 
-const me = new Person();
+const me = {}
+Object.setPrototypeOf(me, person)
 console.log(me.talk());
-
-class SuperHuman extends Person {
-  fly() {
-    return 'Flying';
-  }
-}
-
-const you = new SuperHuman();
-console.log(you.fly());
-console.log(you.talk());
-console.log(me.fly());
