31e705e176f228fccec6767e538b038a22c78600
Christoph <c.wagner.germany@gmail.com>
2024-06-15 14:51:54 +0200

    inheritance6

--- weather.js
+++ weather.js
@@ -1,18 +1,30 @@
-class Person {
-  talk() {
-    return 'Talking';
-  }
+// class Person {
+//   talk() {
+//     return 'Talking';
+//   }
+// }
+
+// const me = new Person();
+// const you = new Person();
+
+// console.log(me.talk());
+// console.log(you.talk());
+
+// Person.prototype.talk = function () {
+//   return 'New Talk';
+// };
+
+// console.log(me.talk());
+// console.log(you.talk());
+
+function Person() {
+  this.age = 12;
 }
 
 const me = new Person();
-const you = new Person();
 
-console.log(me.talk());
-console.log(you.talk());
+console.log(me.age);
 
-Person.prototype.talk = function () {
-  return 'New Talk';
-};
+Person.age = 40;
 
-console.log(me.talk());
-console.log(you.talk());
+console.log(me.age);
