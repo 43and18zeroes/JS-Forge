0e50773cd4c722808b7cefc086a874465569252b
Christoph <c.wagner.germany@gmail.com>
2024-06-17 22:53:37 +0200

    17June8

--- weather.js
+++ weather.js
@@ -45,3 +45,19 @@
 // ben.age = 27;
 // console.log(ben); 
 
+function Dude(name) {
+    this.name = name;
+}
+
+const me = new Dude('Sina');
+console.log(me);
+console.log(me.prototype);
+console.log(me.__proto__);
+console.log(Dude.prototype);
+console.log(me.__proto__ === Dude.prototype);
+
+Dude.prototype.talk = function() {
+    return 'Talking';
+}
+console.log(me.talk());
+console.log(me.__proto__);
