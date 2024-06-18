c6626bcac4b20fa362d687268049b766d9b34a7d
Christoph <c.wagner.germany@gmail.com>
2024-06-18 05:20:50 +0200

    18June3

--- weather.js
+++ weather.js
@@ -16,3 +16,17 @@ function createElement(type, text, color) {
 
 const h1 = createElement('h1', 'Hey guys', 'red');
 console.log(h1);
+
+const me = new Dude('Sina');
+console.log(me);
+console.log(me.prototype);
+console.log(me.__proto__);
+console.log(Dude.prototype);
+console.log(me.__proto__ === Dude.prototype);
+
+
+Dude.prototype.talk = function() {
+    return 'Talking';
+}
+console.log(me.talk());
+console.log(me.__proto__);
