8101965b9116580e79b7a30983828879ba229ca8
Christoph <c.wagner.germany@gmail.com>
2024-06-18 06:08:38 +0200

    18June12

--- weather.js
+++ weather.js
@@ -11,3 +11,12 @@ const array = ['a', 'b', 'c'];
 const myElements = array.map(item => {
   return new SuperElement('p', item);
 });
+console.log(myElements);
+
+
+let zahlen = [1, 2, 3, 4, 5];
+// let verdoppelteZahlen = zahlen.map(function(zahl) {
+//   return zahl * 2;
+// });
+let verdoppelteZahlen = zahlen.map(zahl => zahl * 2);
+console.log(verdoppelteZahlen); // Ausgabe: [2, 4, 6, 8, 10]
