277f5cc3e1af3b5cde32a5bff341e5f63e858fcc
Christoph <c.wagner.germany@gmail.com>
2024-06-19 06:18:55 +0200

    19June2

--- weather.js
+++ weather.js
@@ -1,18 +1,3 @@
-function SuperElement(type, content) {
-  this.el = document.createElement(type);
-  this.el.innerText = content;
-  document.body.append(this.el);
-  this.el.addEventListener('click', () => {
-    console.log(this.el);
-  });
-}
-
-const array = ['a', 'b', 'c'];
-const myElements = array.map(item => {
-  return new SuperElement('p', item);
-});
-console.log(myElements);
-
 function createElement(type, text, color) {
   const el = document.createElement(type);
   el.innerText = text;
