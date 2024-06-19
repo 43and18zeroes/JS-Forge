0125de60e06a3bd6ea71f44f69e31f0c0bc65eac
Christoph <c.wagner.germany@gmail.com>
2024-06-19 07:11:18 +0200

    19June10

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function talk() {
-    return `I am ${this.name}`;
+function talk(lang) {
+    if (lang === 'en') {
+        return `I am ${this.name}`;
+    } else if (lang === 'it') {
+        return `Io sono ${this.name}`;
+    }
 }
 
 const me = {
     name: 'Sina',
 }
 
-console.log(talk.call(me)); // I am Sina
+console.log(talk.call(me, 'en')); // I am Sina
+console.log(talk.call(me, 'it')); // Io sono Sina
