8ff27ebbfc4ac8d6bec53da053244f3b84f5d286
Christoph <c.wagner.germany@gmail.com>
2024-07-02 14:04:36 +0200

    2July4

--- script.js
+++ script.js
@@ -25,25 +25,10 @@ console.log(talk.call(me, 'it', true)); // Ciao bella, sono Sina
 console.log(talk.call(me, 'en', false)); // I am Sina, what you want?
 console.log(talk.call(me, 'it', false)); // Sono Sina, 'angry gesture'
 
-setTimeout(() => {
-  console.log('Dies wird nach 2 Sekunden ausgeführt');
-}, 2000);
+async function fetchData() {
+  const response = await fetch('https://api.example.com/data');
+  const data = await response.json();
+  console.log(data);
+}
 
-let counter = 0;
-const intervalId = setInterval(() => {
-  console.log('Dies wird alle 1 Sekunde ausgeführt');
-  counter++;
-  if (counter === 3) {
-    clearInterval(intervalId);
-  }
-}, 1000);
-
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('Erfolg!');
-  }, 1000);
-});
-
-myPromise.then((value) => {
-  console.log(value);
-});
+fetchData();
