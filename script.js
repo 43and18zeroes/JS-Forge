69b7048dfe00c85dcddd2ce7c1bbc77758a01d59
Christoph <c.wagner.germany@gmail.com>
2024-07-03 12:35:58 +0200

    3July11

--- script.js
+++ script.js
@@ -24,14 +24,3 @@ console.log(talk.call(me, 'en', true)); // Hello, I am Sina
 console.log(talk.call(me, 'it', true)); // Ciao bella, sono Sina
 console.log(talk.call(me, 'en', false)); // I am Sina, what you want?
 console.log(talk.call(me, 'it', false)); // Sono Sina, 'angry gesture'
-
-async function reduceExample() {
-  const urls = ['https://api.example.com/data1', 'https://api.example.com/data2'];
-  const data = await urls.reduce(async (acc, url) => {
-    const response = await fetch(url);
-    const json = await response.json();
-    (await acc).push(json);
-    return acc;
-  }, Promise.resolve([]));
-  console.log(data);
-}
