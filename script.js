acb5f8de701d649a59d13cbb4c2e8dc020ed16a2
Christoph <c.wagner.germany@gmail.com>
2024-07-03 11:54:24 +0200

    3July6

--- script.js
+++ script.js
@@ -25,10 +25,13 @@ console.log(talk.call(me, 'it', true)); // Ciao bella, sono Sina
 console.log(talk.call(me, 'en', false)); // I am Sina, what you want?
 console.log(talk.call(me, 'it', false)); // Sono Sina, 'angry gesture'
 
-async function multipleAwaitExample() {
-  const response1 = await fetch('https://api.example.com/data1');
+async function promiseAllExample() {
+  const promises = [
+    fetch('https://api.example.com/data1'),
+    fetch('https://api.example.com/data2')
+  ];
+  const [response1, response2] = await Promise.all(promises);
   const data1 = await response1.json();
-  const response2 = await fetch('https://api.example.com/data2');
   const data2 = await response2.json();
   console.log(data1, data2);
 }
