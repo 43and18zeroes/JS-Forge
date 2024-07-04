6f3a6cda4847af70ef4b9eb8dd3640beea86ee21
Christoph <c.wagner.germany@gmail.com>
2024-07-04 10:45:11 +0200

    4July4

--- script.js
+++ script.js
@@ -25,12 +25,14 @@ console.log(talk.call(me, 'it', true)); // Ciao bella, sono Sina
 console.log(talk.call(me, 'en', false)); // I am Sina, what you want?
 console.log(talk.call(me, 'it', false)); // Sono Sina, 'angry gesture'
 
-const promise3 = Promise.resolve(3);
-const promise4 = 42;
-const promise5 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 1000, 'foo');
+const promise6 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 500, 'eins');
 });
 
-Promise.all([promise3, promise4, promise5]).then((values) => {
-  console.log(values); // [3, 42, "foo"]
+const promise7 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'zwei');
+});
+
+Promise.race([promise6, promise7]).then((value) => {
+  console.log(value); // "zwei"
 });
