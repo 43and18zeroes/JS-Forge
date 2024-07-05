c5d7eef67ad6d03489a9a7d5e748d36b79adbc43
Christoph <c.wagner.germany@gmail.com>
2024-07-05 11:30:38 +0200

    5July9

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const person = {
-  name: "John",
-  age: 30,
-  address: {
-      city: "New York",
-      zip: "10001"
-  }
-};
+function sum(...numbers) {
+  return numbers.reduce((acc, num) => acc + num, 0);
+}
 
-const { name, age, address: { city } } = person;
-console.log(name); // John
-console.log(age); // 30
-console.log(city); // New York
+console.log(sum(1, 2, 3, 4)); // 10
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combined = [...arr1, ...arr2];
+console.log(combined); // [1, 2, 3, 4, 5, 6]
