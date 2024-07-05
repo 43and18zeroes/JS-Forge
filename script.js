f698fe296d9db217aca791eaf0bed957afda00fe
Christoph <c.wagner.germany@gmail.com>
2024-07-05 11:10:23 +0200

    5July7

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function* fibonacci() {
-  let a = 0, b = 1;
-  while (true) {
-      yield a;
-      [a, b] = [b, a + b];
+const handler = {
+  get: function(target, prop, receiver) {
+      if (prop in target) {
+          return target[prop];
+      } else {
+          throw new Error(`Property ${prop} does not exist.`);
+      }
   }
-}
+};
 
-const gen = fibonacci();
-console.log(gen.next().value); // 0
-console.log(gen.next().value); // 1
-console.log(gen.next().value); // 1
-console.log(gen.next().value); // 2
+const person = {
+  name: "John",
+  age: 30
+};
+
+const proxyPerson = new Proxy(person, handler);
+console.log(proxyPerson.name); // John
+console.log(proxyPerson.age); // 30
+// console.log(proxyPerson.address); // Error: Property address does not exist.
