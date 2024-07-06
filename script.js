2fdb8214ac9475d21f157001447791cce8ac1d6b
Christoph <c.wagner.germany@gmail.com>
2024-07-06 14:34:22 +0200

    6July11

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function promisify(func) {
-  return function(...args) {
-      return new Promise((resolve, reject) => {
-          func(...args, (err, result) => {
-              if (err) return reject(err);
-              resolve(result);
-          });
-      });
-  };
-}
-const fs = require('fs');
-const readFile = promisify(fs.readFile);
-readFile('file.txt', 'utf8')
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+setTimeout(() => {
+  console.log('Diese Nachricht wird nach 2 Sekunden angezeigt');
+}, 2000);
