5e8cac375a134a74d0c985e9cb461ee1d073ac4f
Christoph <c.wagner.germany@gmail.com>
2024-07-06 14:29:21 +0200

    6July10

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-async function fetchData(url) {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
+function promisify(func) {
+  return function(...args) {
+      return new Promise((resolve, reject) => {
+          func(...args, (err, result) => {
+              if (err) return reject(err);
+              resolve(result);
+          });
+      });
+  };
 }
+const fs = require('fs');
+const readFile = promisify(fs.readFile);
+readFile('file.txt', 'utf8')
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
