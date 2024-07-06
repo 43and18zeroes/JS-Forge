934de838c5b3ca3dc964040ab978a555e42c2909
Christoph <c.wagner.germany@gmail.com>
2024-07-06 14:25:12 +0200

    6July9

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function* generatorFunction() {
-  let i = 0;
-  while (true) {
-      yield i++;
-  }
+async function fetchData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
-const gen = generatorFunction();
-console.log(gen.next().value); // 0
-console.log(gen.next().value); // 1
