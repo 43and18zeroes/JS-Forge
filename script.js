d0e46a7692154064bdb1a98a1cbd5be4dff61b4b
Christoph <c.wagner.germany@gmail.com>
2024-07-06 12:09:10 +0200

    6July4

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function curry(f) {
-  return function(a) {
-      return function(b) {
-          return f(a, b);
-      };
+function memoize(func) {
+  const cache = new Map();
+  return function(...args) {
+      const key = JSON.stringify(args);
+      if (cache.has(key)) {
+          return cache.get(key);
+      }
+      const result = func(...args);
+      cache.set(key, result);
+      return result;
   };
 }
-const sum = (a, b) => a + b;
-const curriedSum = curry(sum);
-console.log(curriedSum(1)(2)); // 3
+const fib = memoize(n => n <= 1 ? n : fib(n - 1) + fib(n - 2));
+console.log(fib(10)); // 55
