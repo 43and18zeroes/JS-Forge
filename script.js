19e22b40e26800a12b29a4fd36137be79b5f44db
Christoph <c.wagner.germany@gmail.com>
2024-07-06 12:20:54 +0200

    6July6

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function partial(func, ...fixedArgs) {
-  return function(...remainingArgs) {
-      return func(...fixedArgs, ...remainingArgs);
-  };
-}
-const multiply = (a, b) => a * b;
-const double = partial(multiply, 2);
-console.log(double(5)); // 10
+const compose = (f, g) => x => f(g(x));
+const add1 = x => x + 1;
+const double = x => x * 2;
+const add1ThenDouble = compose(double, add1);
+console.log(add1ThenDouble(2)); // 6
