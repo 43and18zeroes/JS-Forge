a358a5f54f6e0b191fb375ed4d56366a395330ec
Christoph <c.wagner.germany@gmail.com>
2024-07-07 14:29:20 +0200

    7July10

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-function* fibonacci() {
-  let [prev, curr] = [0, 1];
-  while (true) {
-    yield curr;
-    [prev, curr] = [curr, prev + curr];
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(callback);
+  }
+
+  emit(eventName, ...args) {
+    const event = this.events[eventName];
+    if (event) {
+      event.forEach(callback => callback.apply(null, args));
+    }
+  }
+
+  off(eventName, callback) {
+    this.events[eventName] = this.events[eventName].filter(cb => cb !== callback);
   }
 }
 
-const gen = fibonacci();
-console.log(gen.next().value); // 1
-console.log(gen.next().value); // 1
-console.log(gen.next().value); // 2
+const emitter = new EventEmitter();
+const logData = data => console.log('Data received:', data);
+emitter.on('data', logData);
+emitter.emit('data', 'Hello, world!'); // Data received: Hello, world!
+emitter.off('data', logData);
