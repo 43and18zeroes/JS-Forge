64bc1c765a0ae69eae6c48ffe75ce546e0d04e85
Christoph Wagner <cw@m-m-k.de>
2024-07-09 11:07:13 +0200

    9July7

--- script.js
+++ script.js
@@ -267,10 +267,6 @@ try {
 
 // Advanced functions
 
-const numbers = [1, 2, 3];
-const newNumbers = [...numbers, 4, 5];
-
-function sum(...args) {
-  return args.reduce((acc, num) => acc + num, 0);
-}
-console.log(sum(1, 2, 3, 4)); // 10
+const name = 'Alice';
+const greeting = `Hello, ${name}!`;
+console.log(greeting); // Hello, Alice!
