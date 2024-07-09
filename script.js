78c5dc2e93f6c85d892adb35ed1af7f97a78f492
Christoph Wagner <cw@m-m-k.de>
2024-07-09 11:31:13 +0200

    9July9

--- script.js
+++ script.js
@@ -267,5 +267,9 @@ try {
 
 // Advanced functions
 
-const add = (a, b) => a + b;
+// math.js
+export const add = (a, b) => a + b;
+
+// main.js
+import { add } from './math.js';
 console.log(add(2, 3)); // 5
