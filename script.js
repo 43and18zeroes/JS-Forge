c89388f5bd291f603484ea15337dcf3fc8e92be1
Christoph <c.wagner.germany@gmail.com>
2024-07-11 11:45:08 +0200

    11July8

--- script.js
+++ script.js
@@ -258,13 +258,6 @@ try {
 
 // Advanced Functions:
 
-function deepClone(obj) {
-  if (obj === null || typeof obj !== 'object') return obj;
-  const clone = Array.isArray(obj) ? [] : {};
-  for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-          clone[key] = deepClone(obj[key]);
-      }
-  }
-  return clone;
+function compose(...funcs) {
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
 }
