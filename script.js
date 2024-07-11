37914682c86dab828ea4e835fb5f9ea134257db0
Christoph <c.wagner.germany@gmail.com>
2024-07-11 09:52:53 +0200

    11July1

--- script.js
+++ script.js
@@ -258,3 +258,15 @@ try {
 
 // Advanced Functions:
 
+function memoize(func) {
+  const cache = new Map();
+  return function(...args) {
+      const key = JSON.stringify(args);
+      if (cache.has(key)) {
+          return cache.get(key);
+      }
+      const result = func(...args);
+      cache.set(key, result);
+      return result;
+  };
+}
