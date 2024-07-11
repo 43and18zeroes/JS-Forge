9bfd23a7836b8c82a20f7ae4b54fb45788b04980
Christoph <c.wagner.germany@gmail.com>
2024-07-11 09:58:58 +0200

    11July3

--- script.js
+++ script.js
@@ -258,10 +258,21 @@ try {
 
 // Advanced Functions:
 
-function debounce(func, wait) {
-  let timeout;
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
   return function(...args) {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func.apply(this, args), wait);
+      if (!lastRan) {
+          func.apply(this, args);
+          lastRan = Date.now();
+      } else {
+          clearTimeout(lastFunc);
+          lastFunc = setTimeout(() => {
+              if ((Date.now() - lastRan) >= limit) {
+                  func.apply(this, args);
+                  lastRan = Date.now();
+              }
+          }, limit - (Date.now() - lastRan));
+      }
   };
 }
