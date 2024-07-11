f2c8a2a48031a3bd890ccae0aa0c21466f064a6d
Christoph <c.wagner.germany@gmail.com>
2024-07-11 12:14:55 +0200

    11July11

--- script.js
+++ script.js
@@ -258,11 +258,3 @@ try {
 
 // Advanced Functions:
 
-function isPlainObject(obj) {
-  if (typeof obj !== 'object' || obj === null) return false;
-  let proto = obj;
-  while (Object.getPrototypeOf(proto) !== null) {
-      proto = Object.getPrototypeOf(proto);
-  }
-  return Object.getPrototypeOf(obj) === proto;
-}
