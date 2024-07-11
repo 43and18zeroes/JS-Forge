409840dad86408200ff04f64c49a56ce683358a2
Christoph <c.wagner.germany@gmail.com>
2024-07-11 09:58:39 +0200

    11July2

--- script.js
+++ script.js
@@ -258,15 +258,10 @@ try {
 
 // Advanced Functions:
 
-function memoize(func) {
-  const cache = new Map();
+function debounce(func, wait) {
+  let timeout;
   return function(...args) {
-      const key = JSON.stringify(args);
-      if (cache.has(key)) {
-          return cache.get(key);
-      }
-      const result = func(...args);
-      cache.set(key, result);
-      return result;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func.apply(this, args), wait);
   };
 }
