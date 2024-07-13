76ba7b038dc9dcef8108157d446b8b1bfa31e9c8
Christoph <c.wagner.germany@gmail.com>
2024-07-13 13:56:49 +0200

    13July5

--- app.js
+++ app.js
@@ -43,25 +43,26 @@ button.addEventListener('click', trackUserHandler);
 
 // Advanced functions
 
-// curry.js
-function curry(fn) {
-  return function curried(...args) {
-    if (args.length >= fn.length) {
-      return fn.apply(this, args);
-    } else {
-      return function(...args2) {
-        return curried.apply(this, args.concat(args2));
-      };
-    }
+// promisify.js
+function promisify(fn) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
+    });
   };
 }
 
 // Example usage:
-function sum(a, b, c) {
-  return a + b + c;
+function asyncFunction(value, callback) {
+  setTimeout(() => {
+    callback(null, `Result: ${value}`);
+  }, 1000);
 }
 
-const curriedSum = curry(sum);
-console.log(curriedSum(1)(2)(3)); // 6
-console.log(curriedSum(1, 2)(3)); // 6
-console.log(curriedSum(1)(2, 3)); // 6
+const promisedFunction = promisify(asyncFunction);
+promisedFunction('test')
+  .then(result => console.log(result)) // Result: test
+  .catch(err => console.error(err));
