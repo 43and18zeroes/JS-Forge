e8162e51e1ae0ab51baeeb40df3e4213d0e2e261
Christoph <c.wagner.germany@gmail.com>
2024-07-14 20:32:09 +0200

    14July11

--- app.js
+++ app.js
@@ -43,25 +43,3 @@ button.addEventListener('click', trackUserHandler);
 
 // Advanced functions
 
-const throttled = throttle(() => console.log('Throttled!'), 1000);
-const debounced = debounce(() => console.log('Debounced!'), 1000);
-const curriedAdd = curry((a, b, c) => a + b + c);
-const memoizedFib = memoize(function(n) {
-  if (n <= 1) return n;
-  return memoizedFib(n - 1) + memoizedFib(n - 2);
-});
-const flattenArray = flatten([1, [2, [3, 4], 5], 6]);
-const uniqueArray = unique([1, 2, 2, 3, 4, 4, 5]);
-const grouped = groupBy([{name: 'Alice', age: 21}, {name: 'Bob', age: 22}, {name: 'Charlie', age: 21}], 'age');
-const piped = pipe(x => x * 2, x => x + 1)(5);
-const composed = compose(x => x * 2, x => x + 1)(5);
-
-console.log('Throttled:', throttled);
-console.log('Debounced:', debounced);
-console.log('Curried Add:', curriedAdd(1)(2)(3));
-console.log('Memoized Fib:', memoizedFib(10));
-console.log('Flatten Array:', flattenArray);
-console.log('Unique Array:', uniqueArray);
-console.log('Grouped:', grouped);
-console.log('Piped:', piped);
-console.log('Composed:', composed);
