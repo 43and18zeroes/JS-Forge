9ff61d17df08b2163ba430d8710f5f9d509c598b
Christoph <c.wagner.germany@gmail.com>
2024-07-17 18:43:01 +0200

    17July13

--- app.js
+++ app.js
@@ -43,3 +43,14 @@ button.addEventListener('click', trackUserHandler);
 
 // Advanced functions
 
+function curry(fn) {
+  return function curried(...args) {
+      if (args.length >= fn.length) {
+          return fn.apply(this, args);
+      } else {
+          return function(...args2) {
+              return curried.apply(this, args.concat(args2));
+          };
+      }
+  };
+}
