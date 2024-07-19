8d8a4271339cb0daf1f7a227ee280eb412fb9bfd
Christoph <c.wagner.germany@gmail.com>
2024-07-19 17:45:13 +0200

    19July15

--- js/app.js
+++ js/app.js
@@ -1,68 +1,146 @@
-const button = document.querySelector('button');
-const output = document.querySelector('p');
+const initialStartDate = '2013-10-01';
+const today = new Date();
+today.setDate(today.getDate() - 1);
+const initialEndDate = today.toISOString().split('T')[0];
+const ctx = document.getElementById('btc-chart').getContext('2d');
+let bitcoinData;
+let chartInstance = null;
 
-const getPosition = opts => {
-  const promise = new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      success => {
-        resolve(success);
-      },
-      error => {},
-      opts
-    );
-  });
-  return promise;
+const chartParams = {
+  sevenDays: { labels: [], data: [] },
+  oneMonth: { labels: [], data: [] },
+  sixMonths: { labels: [], data: [] },
+  max: { labels: [], data: [] },
 };
 
-const setTimer = duraction => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Done!');
-    }, duraction);
-  });
-  return promise;
-};
-
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-    .then(posData => {
-      positionData = posData;
-      return setTimer(2000);
-    })
-    .then(data => {
-      console.log(data, positionData);
-    });
-  setTimer(1000).then(() => {
-    console.log('Time done!');
-  });
-  console.log('Getting position...');
+function createGradient(ctx) {
+  const gradient = ctx.createLinearGradient(0, 0, 0, 350);
+  gradient.addColorStop(0, 'rgba(247, 147, 26, 1)'); // Dunklere orange Farbe am Start
+  gradient.addColorStop(1, 'rgba(24, 26, 32, 0.1)'); // Hellere orange Farbe am Ende
+  return gradient;
 }
 
-button.addEventListener('click', trackUserHandler);
-
-// Advanced functions
-
-document.addEventListener('DOMContentLoaded', function () {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+function drawChart(labels, data) {
+  if (!chartInstance) {
+    const gradient = createGradient(ctx);
+    chartInstance = new Chart(ctx, {
       type: 'line',
       data: {
-          labels: ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'],
-          datasets: [{
-              label: 'Aktienkurs',
-              data: [100, 120, 110, 130, 140],
-              backgroundColor: 'rgba(255, 99, 132, 0.2)',
-              borderColor: 'rgba(255, 99, 132, 1)',
-              borderWidth: 1
-          }]
+        labels: labels,
+        datasets: [
+          {
+            label: 'BITCOIN - EURO (BTC - EUR)',
+            data: data,
+            // backgroundColor: 'rgba(247, 147, 26, 1)',
+            backgroundColor: gradient,
+            borderColor: 'rgba(247, 147, 26, 1)',
+            borderWidth: 2,
+            pointRadius: 0, // Keine Kreise anzeigen
+            fill: true,
+          },
+        ],
       },
       options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
+        scales: {
+          y: {
+            beginAtZero: false,
+            grid: {
+              color: '#979da8',
+            },
+            ticks: {
+              color: '#979da8',
+            },
+          },
+          x: {
+            grid: {
+              color: '#979da8',
+            },
+            ticks: {
+              color: '#979da8',
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: '#979da8',
+            },
+          },
+        },
+      },
+    });
+  } else {
+    chartInstance.data.labels = labels;
+    chartInstance.data.datasets[0].data = data;
+    chartInstance.update();
+  }
+}
+
+function initChartParams() {
+  chartParams.sevenDays.labels = Object.keys(bitcoinData).slice(-7);
+  chartParams.sevenDays.data = Object.values(bitcoinData).slice(-7);
+  chartParams.sixMonths.labels = Object.keys(bitcoinData).slice(-185);
+  chartParams.sixMonths.data = Object.values(bitcoinData).slice(-185);
+  for (let i = 0; i < Object.keys(bitcoinData).length; i += 48) {
+    chartParams.max.labels.push(Object.keys(bitcoinData)[i]);
+    chartParams.max.data.push(Object.values(bitcoinData)[i]);
+  }
+}
+
+function addButtonEventListener(buttonId, labels, data) {
+  const button = document.getElementById(buttonId);
+  button.addEventListener('click', () => {
+    drawChart(labels, data);
   });
-});
+}
+
+function initBtnEventlisteners() {
+  addButtonEventListener(
+    'seven-days-btn',
+    chartParams.sevenDays.labels,
+    chartParams.sevenDays.data
+  );
+  addButtonEventListener(
+    'one-month-btn',
+    chartParams.oneMonth.labels,
+    chartParams.oneMonth.data
+  );
+  addButtonEventListener(
+    'six-month-btn',
+    chartParams.sixMonths.labels,
+    chartParams.sixMonths.data
+  );
+  addButtonEventListener(
+    'max-btn',
+    chartParams.max.labels,
+    chartParams.max.data
+  );
+}
+
+async function fetchBitcoinData(startDate, endDate, currency = 'EUR') {
+  const url = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error('Network response was not ok ' + response.statusText);
+  }
+  const data = await response.json();
+  return data.bpi;
+}
+
+async function initChart() {
+  try {
+    bitcoinData = await fetchBitcoinData(initialStartDate, initialEndDate);
+    chartParams.oneMonth.labels = Object.keys(bitcoinData).slice(-31);
+    chartParams.oneMonth.data = Object.values(bitcoinData).slice(-31);
+    drawChart(chartParams.oneMonth.labels, chartParams.oneMonth.data);
+    initChartParams();
+    initBtnEventlisteners();
+  } catch (error) {
+    console.error('Error fetching Bitcoin data:', error);
+    alert('Error loading the Bitcoin data. Please try again later.');
+  }
+}
+
+initChart();
+
+// clone
