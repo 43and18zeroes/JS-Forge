ac495054fad790c4837d3657115d78cc2db54f1b
Christoph <c.wagner.germany@gmail.com>
2024-07-22 19:31:06 +0200

    22July11

--- js/app.js
+++ js/app.js
@@ -144,34 +144,3 @@ async function initChart() {
 initChart();
 
 // clone
-
-// eventEmitter.js
-class EventEmitter {
-  constructor() {
-    this.events = {};
-  }
-
-  on(event, listener) {
-    if (!this.events[event]) {
-      this.events[event] = [];
-    }
-    this.events[event].push(listener);
-  }
-
-  emit(event, ...args) {
-    if (this.events[event]) {
-      this.events[event].forEach(listener => listener(...args));
-    }
-  }
-
-  removeListener(event, listener) {
-    if (this.events[event]) {
-      this.events[event] = this.events[event].filter(l => l !== listener);
-    }
-  }
-}
-
-// Example usage:
-const emitter = new EventEmitter();
-emitter.on('greet', name => console.log(`Hello, ${name}!`));
-emitter.emit('greet', 'World'); // Hello, World!
