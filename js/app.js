f6640843c62af8582eeaafb624d05d95f8c519f3
Christoph <c.wagner.germany@gmail.com>
2024-07-25 10:18:18 +0200

    25July6

--- js/app.js
+++ js/app.js
@@ -145,19 +145,17 @@ initChart();
 
 // clone
 
-function deepClone(obj) {
-  if (obj === null || typeof obj !== 'object') {
-      return obj;
-  }
-  const clone = Array.isArray(obj) ? [] : {};
-  for (let key in obj) {
-      if (obj.hasOwnProperty(key)) {
-          clone[key] = deepClone(obj[key]);
-      }
-  }
-  return clone;
+if (!Promise.allSettled) {
+  Promise.allSettled = function(promises) {
+      return Promise.all(promises.map(p => Promise.resolve(p).then(value => ({
+          status: 'fulfilled',
+          value
+      }), reason => ({
+          status: 'rejected',
+          reason
+      }))));
+  };
 }
 
-const original = { a: 1, b: { c: 2 } };
-const cloned = deepClone(original);
-console.log(cloned); // { a: 1, b: { c: 2 } }
+const promises = [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)];
+Promise.allSettled(promises).then(results => console.log(results));
