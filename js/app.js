53ab5af29c4dff2f7d92ed6fba9e1e82b2221f42
Christoph <c.wagner.germany@gmail.com>
2024-07-27 17:40:13 +0200

    27July4

--- js/app.js
+++ js/app.js
@@ -145,15 +145,8 @@ initChart();
 
 // clone
 
-function debounce(fn, delay) {
-  let timeoutId;
-  return function(...args) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn.apply(this, args), delay);
-  };
-}
-
-const handleResize = debounce(() => {
-  console.log('Resized');
-}, 300);
-window.addEventListener('resize', handleResize);
+Promise.allSettled([
+  Promise.resolve(1),
+  Promise.reject(new Error('Error')),
+  Promise.resolve(3)
+]).then(results => console.log(results));
