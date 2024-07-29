e728c7a2c33345575ca07943c547c40b4f7b6fae
Christoph <c.wagner.germany@gmail.com>
2024-07-29 07:56:06 +0200

    29July1

--- js/app.js
+++ js/app.js
@@ -144,3 +144,19 @@ async function initChart() {
 initChart();
 
 // clone
+
+function curry(fn) {
+  return function curried(...args) {
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+}
+
+const add = (a, b) => a + b;
+const curriedAdd = curry(add);
+console.log(curriedAdd(1)(2)); // 3
