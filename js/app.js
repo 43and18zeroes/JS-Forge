b814dcc83d0b8d44ded22feb485ab997e75d1f36
Christoph <c.wagner.germany@gmail.com>
2024-07-31 13:37:49 +0200

    31July3

--- js/app.js
+++ js/app.js
@@ -145,21 +145,64 @@ initChart();
 
 // clone
 
-function memoize(fn) {
-  const cache = new Map();
-  return function(...args) {
-    const key = JSON.stringify(args);
-    if (cache.has(key)) {
-      return cache.get(key);
-    }
-    const result = fn.apply(this, args);
-    cache.set(key, result);
-    return result;
-  };
+const content = [
+  [
+    'React is extremely popular',
+    'It makes building complex, interactive UIs a breeze',
+    "It's powerful & flexible",
+    'It has a very active and versatile ecosystem',
+  ],
+  [
+    'Components, JSX & Props',
+    'State',
+    'Hooks (e.g., useEffect())',
+    'Dynamic rendering',
+  ],
+  [
+    'Official web page (react.dev)',
+    'Next.js (Fullstack framework)',
+    'React Native (build native mobile apps with React)',
+  ],
+];
+
+const btnWhyReact = document.getElementById('btn-why-react');
+const btnCoreFeature = document.getElementById('btn-core-features');
+const btnResources = document.getElementById('btn-resources');
+const tabContent = document.getElementById('tab-content');
+
+function displayContent(items) {
+  let listContent = '';
+  for (const item of items) {
+    listContent += `<li>${item}</li>`;
+  }
+  const list = document.createElement('ul');
+  tabContent.innerHTML = ''; // clear existing content
+  list.innerHTML = listContent; // insert new content
+  tabContent.append(list);
 }
 
-const fib = memoize(function(n) {
-  if (n <= 1) return n;
-  return fib(n - 1) + fib(n - 2);
-});
-console.log(fib(10)); // 55
+function highlightButton(btn) {
+  // Clear all existing styling / highlights
+  btnWhyReact.className = '';
+  btnCoreFeature.className = '';
+  btnResources.className = '';
+  btn.className = 'active'; // set new style / highlight
+}
+
+function handleClick(event) {
+  const btnId = event.target.id;
+  highlightButton(event.target);
+  if (btnId === 'btn-why-react') {
+    displayContent(content[0]);
+  } else if (btnId === 'btn-core-features') {
+    displayContent(content[1]);
+  } else {
+    displayContent(content[2]);
+  }
+}
+
+displayContent(content[0]); // initially show this content
+
+btnWhyReact.addEventListener('click', handleClick);
+btnCoreFeature.addEventListener('click', handleClick);
+btnResources.addEventListener('click', handleClick);
