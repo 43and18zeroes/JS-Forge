f82f4e84c4552dc7915e6d9baf4e5b1525d7ec01
Christoph <c.wagner.germany@gmail.com>
2024-08-03 21:20:02 +0200

    3August5

--- js/app.js
+++ js/app.js
@@ -145,10 +145,8 @@ initChart();
 
 // clone
 
-async function fetchData() {
-  const response = await fetch('https://api.example.com/data');
-  const data = await response.json();
-  console.log(data);
-}
-
-fetchData();
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
