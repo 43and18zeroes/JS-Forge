456e9a7ad8a2dfafbf4fa781887b87faa782eb74
Christoph <c.wagner.germany@gmail.com>
2024-08-06 12:18:26 +0200

    6August6

--- js/app.js
+++ js/app.js
@@ -145,8 +145,11 @@ initChart();
 
 // clone
 
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.example.com/data', true);
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    console.log(JSON.parse(xhr.responseText));
+  }
+};
+xhr.send();
