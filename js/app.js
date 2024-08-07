ce6a23b3657998dd21bf62018d6cc6a87243b9cb
Christoph <c.wagner.germany@gmail.com>
2024-08-07 08:18:02 +0200

    7August1

--- js/app.js
+++ js/app.js
@@ -145,3 +145,12 @@ initChart();
 
 // clone
 
+function fetchData(callback) {
+  setTimeout(() => {
+    callback("Data fetched");
+  }, 1000);
+}
+
+fetchData((data) => {
+  console.log(data);
+});
