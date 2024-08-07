9e655f37cce0e28e2a4f054b4fa9269a6639b6d0
Christoph <c.wagner.germany@gmail.com>
2024-08-07 09:00:42 +0200

    7August8

--- js/app.js
+++ js/app.js
@@ -145,10 +145,12 @@ initChart();
 
 // clone
 
-let counter = 0;
-const intervalId = setInterval(() => {
-  console.log(`Interval message ${++counter}`);
-  if (counter === 5) {
-    clearInterval(intervalId);
-  }
-}, 1000);
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
+});
