e7c7373867392ec7d1253503bad8c68ffb50d658
Christoph <c.wagner.germany@gmail.com>
2024-08-07 08:47:18 +0200

    7August6

--- js/app.js
+++ js/app.js
@@ -145,11 +145,6 @@ initChart();
 
 // clone
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.example.com/data', true);
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    console.log(JSON.parse(xhr.responseText));
-  }
-};
-xhr.send();
+setTimeout(() => {
+  console.log("Delayed message");
+}, 2000);
