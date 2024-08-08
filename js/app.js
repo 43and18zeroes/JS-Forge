95f790f788a185fc2adacbe1e867f76725e4dea3
Christoph <c.wagner.germany@gmail.com>
2024-08-08 11:09:02 +0200

    7August9

--- js/app.js
+++ js/app.js
@@ -145,19 +145,20 @@ initChart();
 
 // clone
 
-function throttle(func, limit) {
-  let inThrottle;
-  return function(...args) {
-      if (!inThrottle) {
-          func.apply(this, args);
-          inThrottle = true;
-          setTimeout(() => inThrottle = false, limit);
+function deepClone(obj) {
+  if (obj === null || typeof obj !== 'object') {
+      return obj;
+  }
+  const clone = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+          clone[key] = deepClone(obj[key]);
       }
-  };
+  }
+  return clone;
 }
 
-const handleScroll = throttle(() => {
-  console.log('Scrolled');
-}, 100);
-
-window.addEventListener('scroll', handleScroll);
+const original = { a: 1, b: { c: 2 } };
+const cloned = deepClone(original);
+console.log(cloned); // { a: 1, b: { c: 2 } }
+console.log(original === cloned); // false
