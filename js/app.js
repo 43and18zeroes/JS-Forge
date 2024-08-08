59d7f970b9a0e61d0f0e1d8e87b4e5af34c99deb
Christoph <c.wagner.germany@gmail.com>
2024-08-08 11:18:46 +0200

    7August11

--- js/app.js
+++ js/app.js
@@ -145,11 +145,3 @@ initChart();
 
 // clone
 
-const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x);
-
-const add = x => x + 1;
-const multiply = x => x * 2;
-const subtract = x => x - 3;
-
-const result = pipe(add, multiply, subtract)(5);
-console.log(result); // 9
