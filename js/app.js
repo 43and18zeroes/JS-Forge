9a4bb59f0c7fec70f6ae047b01fc07f10d84cac2
Christoph <c.wagner.germany@gmail.com>
2024-08-09 13:04:45 +0200

    9August3

--- js/app.js
+++ js/app.js
@@ -145,21 +145,23 @@ initChart();
 
 // clone
 
-function curry(fn) {
-  return function curried(...args) {
-      if (args.length >= fn.length) {
-          return fn.apply(this, args);
-      } else {
-          return function(...args2) {
-              return curried.apply(this, args.concat(args2));
-          }
+function memoize(fn) {
+  const cache = new Map();
+  return function(...args) {
+      const key = JSON.stringify(args);
+      if (cache.has(key)) {
+          return cache.get(key);
       }
+      const result = fn.apply(this, args);
+      cache.set(key, result);
+      return result;
   };
 }
 
-function sum(a, b, c) {
-  return a + b + c;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-const curriedSum = curry(sum);
-console.log(curriedSum(1)(2)(3)); // 6
+const memoizedFibonacci = memoize(fibonacci);
+console.log(memoizedFibonacci(10)); // 55
