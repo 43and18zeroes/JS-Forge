765bc3e55454b6e66210687a24a89b783fd8fbdb
Christoph <c.wagner.germany@gmail.com>
2024-08-10 22:54:28 +0200

    asynch2

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ setTimeout(() => {
 console.log(' 2 ');
 console.log(' 3 ');
 console.log(' 4 ');
+
+function greet(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+function processUserInput(nameOfMyCallbackFunction) {
+  const name = 'Example Name';
+  nameOfMyCallbackFunction(name);
+}
+
+// Here we pass the greet function as a callback to processUserInput
+processUserInput(greet);
