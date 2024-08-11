6bb1ad604163989fa46716d4635135d849b4e007
Christoph <c.wagner.germany@gmail.com>
2024-08-11 08:25:36 +0200

    asynch2

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-function one(call_two) {
+function funcOne(callTwo) {
   console.log('step 1 complete. please call step 2');
+  callTwo();
 }
+
+function funcTwo() {
+  console.log('step 2');
+}
+
+funcOne(funcTwo);
