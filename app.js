21ad520a5928317814718579895f8eb9d532db94
Christoph <c.wagner.germany@gmail.com>
2024-08-11 21:04:05 +0200

    asynch10

--- app.js
+++ app.js
@@ -5,35 +5,35 @@ let stocks = {
   toppings: ['chocolate', 'peanuts'],
 };
 
-let order = (fruitName, callProduction) => {
-  setTimeout(() => {
-    console.log(`2s: ${stocks.Fruits[fruitName]} was selected`);
-    callProduction();
-  }, 2000);
-};
+// let order = (fruitName, callProduction) => {
+//   setTimeout(() => {
+//     console.log(`2s: ${stocks.Fruits[fruitName]} was selected`);
+//     callProduction();
+//   }, 2000);
+// };
 
-let production = () => {
-  setTimeout(() => {
-    console.log('2s: production has started');
-    setTimeout(() => {
-      console.log('4s: the fruit has been chopped');
-      setTimeout(() => {
-        console.log(`5s: ${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
-        setTimeout(() => {
-          console.log('6s: the machine was started');
-          setTimeout(() => {
-            console.log(`8s: ice cream was placed on the ${stocks.holder[0]}`);
-            setTimeout(() => {
-              console.log(`11s: ${stocks.toppings[0]} was added as toppings`);
-              setTimeout(() => {
-                console.log('13s: serve ice cream');
-              }, 2000);
-            }, 3000);
-          }, 2000);
-        }, 1000);
-      }, 1000);
-    }, 2000);
-  }, 0);
-};
+// let production = () => {
+//   setTimeout(() => {
+//     console.log('2s: production has started');
+//     setTimeout(() => {
+//       console.log('4s: the fruit has been chopped');
+//       setTimeout(() => {
+//         console.log(`5s: ${stocks.liquid[0]} and ${stocks.liquid[1]} was added`);
+//         setTimeout(() => {
+//           console.log('6s: the machine was started');
+//           setTimeout(() => {
+//             console.log(`8s: ice cream was placed on the ${stocks.holder[0]}`);
+//             setTimeout(() => {
+//               console.log(`11s: ${stocks.toppings[0]} was added as toppings`);
+//               setTimeout(() => {
+//                 console.log('13s: serve ice cream');
+//               }, 2000);
+//             }, 3000);
+//           }, 2000);
+//         }, 1000);
+//       }, 1000);
+//     }, 2000);
+//   }, 0);
+// };
 
-order(0, production);
+// order(0, production);
