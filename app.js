a304d707b3a40bb0caad4498fd7e3e0f3dfa7e32
Christoph <c.wagner.germany@gmail.com>
2024-08-12 12:58:41 +0200

    asynch13

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ let stocks = {
 
 let isShopOpen = true;
 
+let toppingsChoice = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Which topping would you love?');
+    }, 3000);
+  });
+};
+
+async function kitchen() {
+  console.log('A');
+  console.log('B');
+  console.log('C');
+
+  await toppingsChoice();
+
+  console.log('D');
+  console.log('E');
+}
+
+kitchen()
+
+console.log('doing the dishes');
+console.log('cleaning the tables');
+console.log('taking other orders');
