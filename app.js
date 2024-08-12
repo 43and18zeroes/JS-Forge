fc30cf546d4b4a55832564501714d681ebaff443
Christoph <c.wagner.germany@gmail.com>
2024-08-12 13:03:21 +0200

    asynch14

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-let stocks = {
-  Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
-  liquid: ['water', 'ice'],
-  holder: ['cone', 'cup', 'stick'],
-  toppings: ['chocolate', 'peanuts'],
-};
+// let stocks = {
+//   Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+//   liquid: ['water', 'ice'],
+//   holder: ['cone', 'cup', 'stick'],
+//   toppings: ['chocolate', 'peanuts'],
+// };
 
-let isShopOpen = true;
+// let isShopOpen = true;
 
 let toppingsChoice = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log('Which topping would you love?');
+      resolve(console.log('Which topping would you love?'));
     }, 3000);
   });
 };
@@ -26,7 +26,7 @@ async function kitchen() {
   console.log('E');
 }
 
-kitchen()
+kitchen();
 
 console.log('doing the dishes');
 console.log('cleaning the tables');
