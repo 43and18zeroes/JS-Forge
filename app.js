223aaf46c1f3cb9982a528530d7ea70e6212526f
Christoph <c.wagner.germany@gmail.com>
2024-08-12 12:49:38 +0200

    asynch12

--- app.js
+++ app.js
@@ -7,34 +7,3 @@ let stocks = {
 
 let isShopOpen = true;
 
-// let order = () => {
-//   return new Promise((resolve, reject) => {
-//     if(true) {
-//       resolve()
-//     }
-//     else {
-//       reject()
-//     }
-//   })
-// }
-
-// order()
-// .then() // resolved
-// .then() // resolved
-// .then() // resolved
-// .catch() // rejected
-// .finally() // resolved or rejected
-
-async function order() {
-  try {
-    await abc;
-  } catch (error) {
-    console.log("abc doesn't exist", error);
-  } finally {
-    console.log('runs code anyways');
-  }
-}
-
-order().then(()=>{
-  console.log('test text');
-})
