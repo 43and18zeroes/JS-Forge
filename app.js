aba2b68a66673a258ef6c306c4b1a1d224167a14
Christoph <c.wagner.germany@gmail.com>
2024-08-14 14:58:22 +0200

    promises3

--- app.js
+++ app.js
@@ -53,13 +53,12 @@ kitchen();
 
 // functions
 
-const myPromise = new Promise((resolve, reject) => {
-  const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject("Fehler!");
-    }, 1000);
-  });
-  
-  myPromise.catch((error) => {
-    console.log(error); // "Fehler!"
-  });
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
+});
+
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values); // [3, 42, "foo"]
+});
