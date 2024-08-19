65cf02465342b0f33f82cc9056d6f2d0d693de8c
Christoph <c.wagner.germany@gmail.com>
2024-08-19 21:53:53 +0200

    promises10

--- app.js
+++ app.js
@@ -53,12 +53,33 @@ kitchen();
 
 // promises 2
 
-function fetchData() {
-  return fetch('https://api.example.com/data')
-      .then(response => response.json());
+class PromiseQueue {
+  constructor() {
+      this.queue = [];
+      this.running = false;
+  }
+
+  add(promise) {
+      this.queue.push(promise);
+      if (!this.running) {
+          this.run();
+      }
+  }
+
+  async run() {
+      this.running = true;
+      while (this.queue.length) {
+          const promise = this.queue.shift();
+          try {
+              await promise();
+          } catch (error) {
+              console.error('Error:', error);
+          }
+      }
+      this.running = false;
+  }
 }
 
-fetchData()
-  .then(data => console.log('Fetched data:', data))
-  .catch(error => console.error('Error:', error))
-  .finally(() => console.log('Cleanup tasks'));
+const queue = new PromiseQueue();
+queue.add(() => fetch('https://api.example.com/data1').then(response => response.json()));
+queue.add(() => fetch('https://api.example.com/data2').then(response => response.json()));
