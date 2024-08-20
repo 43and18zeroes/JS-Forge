c9abe8dbc2068e68e686be54bb90fbd1e8c8865c
Christoph <c.wagner.germany@gmail.com>
2024-08-20 12:00:26 +0200

    rng7

--- app.js
+++ app.js
@@ -53,11 +53,8 @@ kitchen();
 
 // rng
 
-function getRandomGaussian(mean, stdDev) {
-  let u = 0, v = 0;
-  while (u === 0) u = Math.random(); // Converting [0,1) to (0,1)
-  while (v === 0) v = Math.random();
-  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-  return num * stdDev + mean;
+function getRandomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
-console.log(getRandomGaussian(0, 1));
+let randomDate = getRandomDate(new Date(2020, 0, 1), new Date(2023, 0, 1));
+console.log(randomDate);
