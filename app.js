13c4bd721ba95cdcee96f41d92a77cf83531423c
Christoph <c.wagner.germany@gmail.com>
2024-08-21 12:34:49 +0200

    rng3

--- app.js
+++ app.js
@@ -53,7 +53,5 @@ kitchen();
 
 // rng
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-console.log(getRandomInt(1, 10));
+let randomBoolean = Math.random() < 0.5;
+console.log(randomBoolean);
