3744d78e93b0f4c1a9abaa74bd78578227473155
Christoph <c.wagner.germany@gmail.com>
2024-08-26 21:07:32 +0200

    fin4

--- app.js
+++ app.js
@@ -53,8 +53,6 @@ kitchen();
 
 // finance
 
-function calculateLoanPayment(principal, rate, term) {
-  const monthlyRate = rate / 100 / 12;
-  const numberOfPayments = term * 12;
-  return (principal * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -numberOfPayments));
+function calculateFutureValue(presentValue, rate, n) {
+  return presentValue * Math.pow(1 + (rate / 100), n);
 }
