eb0b066d65b3668f414ecf08a47c880ac1651e3c
Christoph <c.wagner.germany@gmail.com>
2024-08-27 20:06:36 +0200

    fin2

--- app.js
+++ app.js
@@ -53,9 +53,9 @@ kitchen();
 
 // fin 
 
-function calculateSimpleInterest(principal, rate, time) {
-  return principal * rate * time;
+function calculateCompoundInterest(principal, rate, time, compoundingFrequency) {
+  return principal * Math.pow(1 + rate / compoundingFrequency, compoundingFrequency * time);
 }
 
-const interest = calculateSimpleInterest(1000, 0.05, 2);
-console.log("Simple Interest:", interest);
+const compoundInterest = calculateCompoundInterest(1000, 0.05, 2, 12);
+console.log("Compound Interest:", compoundInterest);
