2d914ba52baafff2b0496f577fbc3dffba710db3
Christoph <c.wagner.germany@gmail.com>
2024-08-31 15:57:04 +0200

    loc3

--- app.js
+++ app.js
@@ -53,11 +53,12 @@ kitchen();
 
 // loc
 
-function findElement(arr, value) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === value) {
-      return i;
-    }
+function fibonacci(n) {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
-  return -1;
 }
