70f74ca6e98ceafe0c1631ff9ac64cabbcb567b6
Christoph <c.wagner.germany@gmail.com>
2024-08-31 17:39:17 +0200

    loc12

--- app.js
+++ app.js
@@ -50,19 +50,3 @@ async function kitchen() {
 }
 
 kitchen();
-
-// loc
-
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap elements
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
