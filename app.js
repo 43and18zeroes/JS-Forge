27bf965f3780724d387691c9cd7a311d9c4e2381
Christoph <c.wagner.germany@gmail.com>
2024-08-31 17:00:46 +0200

    loc8

--- app.js
+++ app.js
@@ -53,16 +53,13 @@ kitchen();
 
 // loc
 
-function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap elements
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
+function createElement(tag, attributes, text) {
+  const element = document.createElement(tag);
+  for (const attribute in attributes) {
+    element.setAttribute(attribute, attributes[attribute]);
   }
-  return arr;
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
 }
