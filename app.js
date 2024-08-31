624a5983fd097644525bb9c91b09634217fd5299
Christoph <c.wagner.germany@gmail.com>
2024-08-31 17:34:35 +0200

    loc11

--- app.js
+++ app.js
@@ -53,7 +53,16 @@ kitchen();
 
 // loc
 
-function formatDate(date) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('de-DE', options);
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
